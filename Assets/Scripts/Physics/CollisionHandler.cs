07276c443f635069cb1bd731aba80adf6b371f83
unknown <Mad@LAPTOP-4MHG3LJF>
2019-10-01 17:19:43 +0200

    Begin Physics and collisions
    Add CollisionHandler
    	detects collisions by raycasting
    Add PhysicsHandler
    	handles physics (speed, gravity...)

---
+++ Assets/Scripts/Physics/CollisionHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    [SerializeField]
+    private Vector2[] castPoints;
+    private LayerMask terrainMask;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        Debug.Assert(_rigidbody2D != null, "You must add a Rigidbody2D to the object");
+        Debug.Assert(_rigidbody2D.isKinematic, "The Rigidbody2D must be Kinematic !");
+        terrainMask = LayerMask.GetMask("Terrain");
+    }
+    
+    public Vector2 CorrectMovement(Vector2 trajectory)
+    {
+        Vector2 position = transform.position;
+        for (int i = 0; i < castPoints.Length; i++)
+        {
+            var cast = Physics2D.Raycast(position+castPoints[i], trajectory, trajectory.magnitude, terrainMask);
+        }
+        return trajectory;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector2 position = transform.position;
+        for (int i = 0; i < castPoints.Length; i++)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawCube(position+castPoints[i], new Vector3(0.1f,0.1f));
+        }
+    }
+}
---
+++ Assets/Scripts/Physics/PhysicsHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsHandler : MonoBehaviour
+{
+    private CollisionHandler _collisionHandler;
+    
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private Vector2 _speed;
+    public Vector2 Speed
+    {
+        get
+        {
+            return _speed;
+        }
+        set
+        {
+            _speed = value;
+            if (_speed.magnitude > maxSpeed)
+            {
+                _speed = value.normalized;
+                var sqr = Mathf.Sqrt(maxSpeed);
+                _speed.Scale(new Vector2(sqr, sqr));
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        _collisionHandler = GetComponent<CollisionHandler>();
+        Debug.Assert(_collisionHandler != null);
+    }
+
+    void Update()
+    {
+    }
+}
